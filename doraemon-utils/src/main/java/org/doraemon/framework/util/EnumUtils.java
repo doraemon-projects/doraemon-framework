713a12ce652209d0f40f1bbe13bdc0c68400af1e
fengwenping <244160190@163.com>
2019-12-15 23:19:32 +0800

    增加工具类

---
+++ doraemon-utils/src/main/java/org/doraemon/framework/util/EnumUtils.java
@@ -0,0 +1,35 @@
+package org.doraemon.framework.util;
+
+import java.util.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: 描述
+ * Author:      fengwenping
+ * Date:        2019/12/15 23:10
+ */
+public class EnumUtils {
+
+    public static <E extends Enum<E>> Map<String, E> getEnumMap(final Class<E> enumClass) {
+        final Map<String, E> map = new LinkedHashMap<>();
+        for (final E e : enumClass.getEnumConstants()) {
+            map.put(e.name(), e);
+        }
+        return map;
+    }
+
+    public static <E extends Enum<E>> List<E> getEnumList(final Class<E> enumClass) {
+        return new ArrayList<>(Arrays.asList(enumClass.getEnumConstants()));
+    }
+
+    public static <E extends Enum<E>> E getEnum(final Class<E> enumClass, final String enumName) {
+        if (enumName == null) {
+            return null;
+        }
+        try {
+            return Enum.valueOf(enumClass, enumName);
+        } catch (final IllegalArgumentException ex) {
+            return null;
+        }
+    }
+}
