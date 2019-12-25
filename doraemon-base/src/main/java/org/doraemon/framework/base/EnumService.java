91364fe96ba310f39dd4466f9386725be275a03f
664558420@qq.com <lin345640>
2019-12-25 17:49:27 +0800

    feat：枚举名称修改

---
+++ doraemon-base/src/main/java/org/doraemon/framework/base/EnumService.java
@@ -0,0 +1,79 @@
+package org.doraemon.framework.base;
+
+/**
+ * 枚举公共接口
+ * 
+ * @author : yuanDong.lin
+ * @date : 2019-12-25 17:45
+ */
+public interface EnumService<K, V> {
+
+    /**
+     * 创建枚举公共接口
+     *
+     * @param k 枚举名
+     * @param v 枚举值
+     * @param <K> 枚举名类型
+     * @param <V> 枚举值类型
+     * @return EnumService 返回枚举
+     */
+    static <K, V> EnumService create(K k, V v) {
+        return new EnumServiceImpl<>(k, v);
+    }
+
+    ;
+
+    /**
+     * 抽象方法，子类按需实现
+     *
+     * @return EnumService
+     */
+    EnumService<K, V> getEnumServiceImpl();
+
+    /**
+     * 获取枚举key
+     *
+     * @return <K>枚举名
+     */
+    default K getKey() {
+        return getEnumServiceImpl().getKey();
+    }
+
+    /**
+     * 获取枚举val
+     *
+     * @return <V>枚举值
+     */
+    default V getVal() {
+        return getEnumServiceImpl().getVal();
+    }
+
+
+}
+
+class EnumServiceImpl<K, V> implements EnumService<K, V> {
+
+    private V v;
+
+    private K k;
+
+    EnumServiceImpl(K k, V v) {
+        this.k = k;
+        this.v = v;
+    }
+
+    @Override
+    public EnumService<K, V> getEnumServiceImpl() {
+        return this;
+    }
+
+    @Override
+    public K getKey() {
+        return k;
+    }
+
+    @Override
+    public V getVal() {
+        return v;
+    }
+}
