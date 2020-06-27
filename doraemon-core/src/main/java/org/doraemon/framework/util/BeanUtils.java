cec927776874ba14589715284852f09a0aae6be4
fengwenping <244160190@163.com>
2020-06-27 19:33:39 +0800

    Bean复制工具

---
+++ doraemon-core/src/main/java/org/doraemon/framework/util/BeanUtils.java
@@ -0,0 +1,78 @@
+package org.doraemon.framework.util;
+
+import net.sf.cglib.beans.BeanCopier;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @description: Bean属性copy工具类
+ * @author: fengwenping
+ * @date: 2020-06-27 19:15
+ */
+public abstract class BeanUtils {
+
+    private BeanUtils() {
+    }
+
+    private static final Map<String, BeanCopier> BEAN_COPIER_CACHE = new ConcurrentHashMap<>();
+
+    public static void copyProperties(Object source, Object target) {
+        BeanCopier copier = getBeanCopier(source.getClass(), target.getClass());
+        copier.copy(source, target, null);
+    }
+
+    private static BeanCopier getBeanCopier(Class sourceClass, Class targetClass) {
+        String beanKey = generateKey(sourceClass, targetClass);
+        BeanCopier copier = null;
+        if (!BEAN_COPIER_CACHE.containsKey(beanKey)) {
+            copier = BeanCopier.create(sourceClass, targetClass, false);
+            BEAN_COPIER_CACHE.put(beanKey, copier);
+        } else {
+            copier = BEAN_COPIER_CACHE.get(beanKey);
+        }
+        return copier;
+    }
+
+    /**
+     * 两个类的全限定名拼接起来构成Key
+     *
+     * @param sourceClass
+     * @param targetClass
+     * @return
+     */
+    private static String generateKey(Class<?> sourceClass, Class<?> targetClass) {
+        return sourceClass.getName() + targetClass.getName();
+    }
+
+    public static <T> T copyProperties(Object source, Class<T> targetClass) {
+        T t = null;
+        try {
+            t = targetClass.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(String.format("Create new instance of %s failed: %s", targetClass, e.getMessage()));
+        }
+        copyProperties(source, t);
+        return t;
+    }
+
+    public static <T> List<T> copyPropertiesOfList(List<?> sourceList, Class<T> targetClass) {
+        if (sourceList == null || sourceList.isEmpty()) {
+            return Collections.emptyList();
+        }
+        List<T> resultList = new ArrayList<>(sourceList.size());
+        for (Object o : sourceList) {
+            T t = null;
+            try {
+                t = copyProperties(o, targetClass);
+                resultList.add(t);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return resultList;
+    }
+}
