5d288b22ec99cffbd00e6f8bd557dd7096db85ad
fengwenping <244160190@163.com>
2020-07-04 18:02:08 +0800

    springboot版本的jaxrs框架搭建完成

---
+++ doraemon-jaxrs-extension/src/main/java/org/doraemon/framework/jaxrs/UnifiedJacksonJaxbJsonProvider.java
@@ -0,0 +1,21 @@
+package org.doraemon.framework.jaxrs;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import org.doraemon.framework.Constants;
+
+import javax.inject.Named;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * @description: 描述
+ * @author: fengwenping
+ * @date: 2020-07-04 17:38
+ */
+@Named("unifiedJacksonXMLProvider")
+@Provider
+@Produces(Constants.ContentType.ALL_VALUE)
+@Consumes(Constants.ContentType.ALL_VALUE)
+public class UnifiedJacksonJaxbJsonProvider extends JacksonJaxbJsonProvider {
+}
---
+++ doraemon-jaxrs-extension/src/main/java/org/doraemon/framework/jaxrs/UnifiedJacksonJaxbXMLProvider.java
@@ -0,0 +1,22 @@
+package org.doraemon.framework.jaxrs;
+
+import com.fasterxml.jackson.jaxrs.xml.JacksonJaxbXMLProvider;
+import org.doraemon.framework.Constants;
+
+import javax.inject.Named;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * @description: 描述
+ * @author: fengwenping
+ * @date: 2020-07-04 17:39
+ */
+
+@Named("unifiedJacksonJaxbJsonProvider")
+@Provider
+@Produces(Constants.ContentType.ALL_VALUE)
+@Consumes(Constants.ContentType.ALL_VALUE)
+public class UnifiedJacksonJaxbXMLProvider extends JacksonJaxbXMLProvider {
+}
