de65a9798c8f2cfdb99e6bf3d9e35acac361c694
fengwenping <244160190@163.com>
2020-07-05 23:23:39 +0800

    增加断言工具类

--- doraemon-core/src/test/java/org/doraemon/framework/exception/BusinessExceptionTest.java
+++ doraemon-core/src/test/java/org/doraemon/framework/exception/BusinessExceptionTest.java
@@ -2,152 +2,12 @@ package org.doraemon.framework.exception;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /**
  * @description: 描述
  * @author: fengwenping
- * @date: 2020-07-05 22:29
+ * @date: 2020-07-05 23:23
  */
 public class BusinessExceptionTest {
-
-    @Test
-    public void assertTrue() {
-        BusinessException.assertTrue(false);
-    }
-
-    @Test
-    public void testAssertTrue() {
-        BusinessException.assertTrue(true, "未达到预期值");
-    }
-
-    @Test
-    public void assertFalse() {
-        BusinessException.assertFalse(false);
-    }
-
-    @Test
-    public void testAssertFalse() {
-        BusinessException.assertFalse(false, "未达到预期值");
-    }
-
-    @Test
-    public void assertEquals() {
-        BusinessException.assertEquals(1, 0);
-    }
-
-    @Test
-    public void testAssertEquals() {
-        BusinessException.assertEquals(1, 1, "未达到预期值");
-    }
-
-    @Test
-    public void assertNonNull() {
-        BusinessException.assertNotNull(1);
-    }
-
-    @Test
-    public void testAssertNonNull() {
-        BusinessException.assertNotNull(null, "未达到预期值");
-    }
-
-    @Test
-    public void assertNull() {
-        BusinessException.assertNull(1);
-    }
-
-    @Test
-    public void testAssertNull() {
-        BusinessException.assertNull(null, "未达到预期值");
-    }
-
-    @Test
-    public void newException() {
-    }
-
-    @Test
-    public void testNewException() {
-    }
-
-    @Test
-    public void testAssertTrue1() {
-    }
-
-    @Test
-    public void testAssertTrue2() {
-    }
-
-    @Test
-    public void testAssertFalse1() {
-    }
-
-    @Test
-    public void testAssertFalse2() {
-    }
-
-    @Test
-    public void testAssertEquals1() {
-    }
-
-    @Test
-    public void testAssertEquals2() {
-    }
-
-    @Test
-    public void assertNotNull() {
-    }
-
-    @Test
-    public void testAssertNotNull() {
-    }
-
-    @Test
-    public void testAssertNull1() {
-    }
-
-    @Test
-    public void testAssertNull2() {
-    }
-
-    @Test
-    public void assertNotEmpty() {
-    }
-
-    @Test
-    public void testAssertNotEmpty() {
-    }
-
-    @Test
-    public void testAssertNotEmpty1() {
-    }
-
-    @Test
-    public void testAssertNotEmpty2() {
-    }
-
-    @Test
-    public void testAssertNotEmpty3() {
-    }
-
-    @Test
-    public void testAssertNotEmpty4() {
-    }
-
-    @Test
-    public void assertInstanceOf() {
-    }
-
-    @Test
-    public void testAssertInstanceOf() {
-    }
-
-    @Test
-    public void assertIsAssignable() {
-    }
-
-    @Test
-    public void testAssertIsAssignable() {
-    }
 
     @Test
     public void throwNewException() {
@@ -155,5 +15,89 @@ public class BusinessExceptionTest {
 
     @Test
     public void testThrowNewException() {
+    }
+
+    @Test
+    public void testThrowNewException1() {
+    }
+
+    @Test
+    public void isTrue() {
+    }
+
+    @Test
+    public void testIsTrue() {
+    }
+
+    @Test
+    public void isFalse() {
+    }
+
+    @Test
+    public void testIsFalse() {
+    }
+
+    @Test
+    public void isEquals() {
+    }
+
+    @Test
+    public void testIsEquals() {
+    }
+
+    @Test
+    public void isNotNull() {
+    }
+
+    @Test
+    public void testIsNotNull() {
+    }
+
+    @Test
+    public void isNull() {
+    }
+
+    @Test
+    public void testIsNull() {
+    }
+
+    @Test
+    public void isNotEmpty() {
+    }
+
+    @Test
+    public void testIsNotEmpty() {
+    }
+
+    @Test
+    public void testIsNotEmpty1() {
+    }
+
+    @Test
+    public void testIsNotEmpty2() {
+    }
+
+    @Test
+    public void testIsNotEmpty3() {
+    }
+
+    @Test
+    public void testIsNotEmpty4() {
+    }
+
+    @Test
+    public void isInstanceOf() {
+    }
+
+    @Test
+    public void testIsInstanceOf() {
+    }
+
+    @Test
+    public void isAssignableFrom() {
+    }
+
+    @Test
+    public void testIsAssignableFrom() {
     }
 }
